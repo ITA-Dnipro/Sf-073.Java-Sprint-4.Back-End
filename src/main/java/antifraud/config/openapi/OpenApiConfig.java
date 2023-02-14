7cb84c77fd62bc60fb5c4528486fa5c5c07dcb6f
penzata <penzata86@yahoo.com>
2023-02-14 03:29:21 +0200

    swagger

--- src/main/java/antifraud/config/openapi/OpenApiConfig.java
+++ src/main/java/antifraud/config/openapi/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @EnableConfigurationProperties(OpenApiProperty.class)
 public class OpenApiConfig {
     private static final String SCHEME_NAME = "basicAuth";
